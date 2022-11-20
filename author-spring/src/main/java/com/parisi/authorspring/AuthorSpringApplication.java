6fdb84a4f6a1811fceb47d3b61341ad3aea28b22
LorenzoGParisi <lorenzogparisi@gmail.com>
2022-11-20 17:18:49 +0100

    First commit: initialized project

---
+++ author-spring/src/main/java/com/parisi/authorspring/AuthorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.parisi.authorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorSpringApplication.class, args);
+	}
+
+}
---
+++ author-spring/src/test/java/com/parisi/authorspring/AuthorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parisi.authorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
